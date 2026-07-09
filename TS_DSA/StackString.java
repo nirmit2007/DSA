487462880244b4926f72d904a20dc5a2f3eaeb63
nirmit2007 <cnirmit07@gmail.com>
2026-07-09 18:25:39 +0530

    Stack

--- TS_DSA/StackString.java
+++ TS_DSA/StackString.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 import java.util.Stack;
 
-public class StackString 
+public class StackString
 {
-     public static void main(String args[])
+    public static void main(String[] args)
     {
         Stack<Character> s = new Stack<>();
 
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
         String str = sc.next();
 
-        for(int i=0;i<str.length();i++)
+        for(int i = 0; i < str.length(); i++)
         {
             s.push(str.charAt(i));
         }
+
         System.out.println(s);
 
+        while(!s.isEmpty())
+        {
+            System.out.print(s.pop());
+        }
     }
-}    
-
+}
