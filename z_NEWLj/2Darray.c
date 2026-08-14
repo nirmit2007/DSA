c359521d39316caac91e15dc9fa9a6ad8b8bdc33
nirmit2007 <cnirmit07@gmail.com>
2026-08-14 12:06:41 +0530

    2d array in college

---
+++ z_NEWLj/2Darray.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[2][3],i,j;
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\nEnter a[%d][%d] : ",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("\n%d",a[i][j]);
+        }
+    }
+}
